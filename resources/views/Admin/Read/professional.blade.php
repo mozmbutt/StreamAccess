d0ce3cddc6b3ed806a5a62c6f4ea759647cdda5e
Moazzam Ali <mozmbutt8@gmail.com>
2021-02-04 01:31:55 +0500

    professional style

--- resources/views/Admin/Read/professional.blade.php
+++ resources/views/Admin/Read/professional.blade.php
@@ -50,7 +50,7 @@
                         <div class="row">
                             <div class="col-12">
                                 <div class="page-title-box d-flex align-items-center justify-content-between">
-                                    <h4 class="mb-0 font-size-18">All Professtionals</h4>
+                                    <h4 class="mb-0 font-size-18">All Professionals</h4>
 
                                     <div class="page-title-right">
                                         <ol class="breadcrumb m-0">
